8ab0fb72f54fb3604f597f88f5bb44e92be399bb
roy-stapp1solutions <roy@stapp1.solutions>
2025-03-30 16:41:38 +0200

    Add www to the allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["bakumetaalbewerking.nl"],
+    allowedHosts: ["bakumetaalbewerking.nl", "www.bakumetaalbewerking.nl"],
   },
 });
